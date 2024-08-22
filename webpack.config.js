3c15b0016005853ed62bf54093bfe52fcb9b5db9
ablfaxl <taghavi.a2023@gmail.com>
2024-08-22 14:13:51 +0330

    🚀feat: add wepack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "LogifyFa",
+    libraryTarget: "umd",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  mode: "production",
+};
