294932ad036f1ba478e03ea9235a4bfb59cbdd64
ablfaxl <taghavi.a2023@gmail.com>
2024-08-22 14:17:52 +0330

    🚀feat: add wepack config

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { default as clientLogger } from "./client-logger";
-export { default as serverLogger } from "./server-logger";
-//# sourceMappingURL=index.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./src/index.ts", // Entry point for your code
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    library: "LogifyFa",
-    libraryTarget: "umd",
+    library: "LogifyFa", // Global variable name for the UMD build
+    libraryTarget: "umd", // Universal Module Definition
+    globalObject: "this", // Ensures compatibility with different environments
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.ts$/, // Apply this rule to TypeScript files
         use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: [".ts", ".js"], // Resolve .ts and .js files
   },
-  mode: "production",
+  mode: "production", // Set to 'development' for easier debugging
 };
